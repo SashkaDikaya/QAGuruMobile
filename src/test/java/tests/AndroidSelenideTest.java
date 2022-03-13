f725ca9e77a31f9eefadd48a4b1af52713858e82
SashkaDikaya <dikayaav@gmail.com>
2022-03-13 16:24:48 +0300

    add androidTest

--- src/test/java/tests/AndroidSelenideTest.java
+++ src/test/java/tests/AndroidSelenideTest.java
@@ -7,16 +7,20 @@ import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
 
 public class AndroidSelenideTest extends TestBase{
 
     @Test
     void searchTest()  {
-
+        step("Type search", () ->{
         $(MobileBy.AccessibilityId("Search Wikipedia")).click();
         $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
-        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
 
+        step("Verify content found", () ->{
+        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
     }
 
 }
