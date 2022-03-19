696014209b59b440918d98450a7c74f657b8934d
SashkaDikaya <dikayaav@gmail.com>
2022-03-19 14:48:19 +0300

    add androidTest

--- src/test/java/tests/AndroidSelenideHomeWorkTest.java
+++ src/test/java/tests/AndroidSelenideHomeWorkTest.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-@Tag("selenide")
+@Tag("mobile")
 public class AndroidSelenideHomeWorkTest extends TestBase{
 
     @Test
