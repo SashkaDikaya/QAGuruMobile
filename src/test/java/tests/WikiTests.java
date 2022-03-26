5311d67dda22cc6d8cfd5c4ad72e1ff28f24ea39
SashkaDikaya <dikayaav@gmail.com>
2022-03-26 14:16:31 +0300

    add wikitest... at last!

--- src/test/java/tests/WikiTests.java
+++ src/test/java/tests/WikiTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -14,42 +15,42 @@ public class WikiTests extends TestBase {
     @Test
     void checkScreenTest() {
         step("Check first screen", () -> {
-            $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("Free Encyclopedia"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView")).shouldHave(text("Free Encyclopedia"));
         });
 
         step("Set russian language", () -> {
-            $(MobileBy.id("org.wikipedia.alpha:id/addLangContainer")).click();
-            $(MobileBy.xpath("//*[@text='ADD LANGUAGE']")).click();
-            $(MobileBy.xpath(("//*[@text='Русский']"))).click();
-            $(MobileBy.xpath(("//*[@content-desc='Navigate up']"))).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/addLangContainer")).click();
+            $(AppiumBy.xpath("//*[@text='ADD LANGUAGE']")).click();
+            $(AppiumBy.xpath(("//*[@text='Русский']"))).click();
+            $(AppiumBy.xpath(("//*[@content-desc='Navigate up']"))).click();
         });
 
         step("Check russian language", () -> {
-            $(MobileBy.xpath(("//*[contains(@text,'Русский')]"))).shouldHave(text("Русский"));
+            $(AppiumBy.xpath(("//*[contains(@text,'Русский')]"))).shouldHave(text("Русский"));
         });
 
         step("Go to second screen", () -> {
-            $(MobileBy.xpath(("//*[@text='CONTINUE']"))).click();
+            $(AppiumBy.xpath(("//*[@text='CONTINUE']"))).click();
         });
 
         step("Check second screen", () -> {
-            $(MobileBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Customize the feed to your interests"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Customize the feed to your interests"));
         });
 
         step("Go to third screen", () -> {
-            $(MobileBy.xpath(("//*[@text='CONTINUE']"))).click();
+            $(AppiumBy.xpath(("//*[@text='CONTINUE']"))).click();
         });
 
         step("Check third screen", () -> {
-            $(MobileBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Login to your Wikipedia account"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Login to your Wikipedia account"));
         });
 
         step("Go to fourth screen", () -> {
-            $(MobileBy.xpath(("//*[@text='CONTINUE']"))).click();
+            $(AppiumBy.xpath(("//*[@text='CONTINUE']"))).click();
         });
 
         step("Check fourth screen", () -> {
-            $(MobileBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Learn more"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/secondaryTextView")).shouldHave(text("Learn more"));
 
         });
     }
